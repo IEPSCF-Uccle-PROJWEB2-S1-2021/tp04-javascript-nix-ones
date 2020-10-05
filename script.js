05785e27d1a7a7795554aaf03e7d863a600ae9d3
nix-ones <beanixon@hotmail.fr>
2020-10-05 10:03:25 +0200

    exo1&2 OK

---
+++ script.js
@@ -0,0 +1,23 @@
+function minimum(a,b){
+
+  if((a==b) ||(a<b)){
+    return a;
+  }
+  else{
+    return b;
+  }
+}
+
+
+function maximum(a,b){
+
+  if((a==b) ||(a>b)){
+    return a;
+  }
+  else{
+    return b;
+  }
+}
+function reduce(){
+
+}
