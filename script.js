f2347b9f6cbec8a866110db4c663f56693db057f
nix-ones <beanixon@hotmail.fr>
2020-10-05 20:51:36 +0200

    TP04 FINAL OK

--- script.js
+++ script.js
@@ -18,6 +18,42 @@ function maximum(a,b){
     return b;
   }
 }
-function reduce(){
+function reduce(numbers, fn){
+  if(numbers.length<2){
+    return null;
+  }
+  else{
+    let score =fn((numbers[0],numbers[1]));
+    if(numbers.length>2);
+    for (let index = 0; index < numbers.length; index++) {
+      score = fn(numbers[index],score);
 
+    }
+    return score;
+  }
 }
+
+let valeur = function filter(numbers,fn){
+  let tab=[] ;
+  let k=0;
+
+  for (let index = 0; index < numbers.length; index++) {
+    if (fn(numbers[index]==true)) {
+      tab[k]==numbers[index];
+      k=k+1;
+    }
+  }
+  return tab;
+}
+
+let myTestNumbers=[ 5, 8, 13, 21, 34, 610, 55, 3, 89, 144, 233, 377];
+console.log(reduce(myTestNumbers,minimum));
+console.log(reduce(myTestNumbers,maximum));
+
+console.log(valeur(myTestNumbers,val=>{
+  if(a>=30){
+    return true;
+      }
+    }
+  )
+);
